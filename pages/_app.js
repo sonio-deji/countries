d6c66829a92a500d0988fb558f55b2b0eb5204a0
atanda uthamn <atandauthman2@gmail.com>
2022-08-07 14:03:47 +0100

    bug fixes

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
   <>
   <Head>
     <style>
-    @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,400;1,900&display=swap');
+   { `@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,400;1,900&display=swap');`}
     </style>
   </Head>
   <Component {...pageProps} />
